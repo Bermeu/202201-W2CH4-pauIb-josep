54d3f86e115cb6b842706f18c524e59c50a0cb18
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-20 22:33:55 +0100

    Added test for creating MockArray

--- index.test.js
+++ index.test.js
@@ -1,4 +1,14 @@
 import { even, MockArray } from "./index.js";
 
-const test = new MockArray();
-console.log(test);
+describe("Given MockArray", () => {
+  describe("When it recives 3", () => {
+    test("It should throw and error with the msg 'Input object lenght cannot be 0'", () => {
+      const expectedError = "Input object lenght cannot be 0";
+      const valueToGive = 3;
+
+      expect(() => {
+        const generatedArrayObject = new MockArray(valueToGive);
+      }).toThrow(expectedError);
+    });
+  });
+});
