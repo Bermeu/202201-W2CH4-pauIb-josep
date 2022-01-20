20fa88f3e049c145e7248541dc11b45d46a6d58f
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-20 23:59:09 +0100

    Added test for function MockArray.find passing even

--- tests/find.test.js
+++ tests/find.test.js
@@ -16,4 +16,20 @@ describe("Given MockArray.find(even)", () => {
       expect(result).toBe(expectedResult);
     });
   });
+
+  describe("When inputed an object with 3 entries that where 1 complies with function even", () => {
+    test("It should return 4", () => {
+      const expectedResult = 4;
+      const valueToGive = {
+        a: 1,
+        b: 3,
+        c: 4,
+      };
+
+      const iniciatedObject = new MockArray(valueToGive);
+      const result = iniciatedObject.find(even);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
 });
