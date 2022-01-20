c62e608dcc841ea829e21cbcacc5e5472e4611d1
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-20 23:26:52 +0100

    Added seccond test for MockArray.some()

--- tests/some.test.js
+++ tests/some.test.js
@@ -16,4 +16,20 @@ describe("Given MockArray.some(even)", () => {
       expect(result).toBe(expectedResult);
     });
   });
+
+  describe("When inputed an object with 3  DOES NOT complies with function even", () => {
+    test("It should return false", () => {
+      const expectedResult = false;
+      const valueToGive = {
+        a: 1,
+        b: 5,
+        c: 3,
+      };
+
+      const iniciatedObject = new MockArray(valueToGive);
+      const result = iniciatedObject.some(even);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
 });
