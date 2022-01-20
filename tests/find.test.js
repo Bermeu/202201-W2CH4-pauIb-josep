66f6241db7ca03d76d2cadce5799fb1a76e34566
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-20 23:42:16 +0100

    Added test for method MockArrayfind() passing function even

---
+++ tests/find.test.js
@@ -0,0 +1,19 @@
+import { even, MockArray } from "../index";
+
+describe("Given MockArray.find(even)", () => {
+  describe("When inputed an object with 3 entries that where 2 comply with function even", () => {
+    test("It should return 2", () => {
+      const expectedResult = 2;
+      const valueToGive = {
+        a: 1,
+        b: 2,
+        c: 4,
+      };
+
+      const iniciatedObject = new MockArray(valueToGive);
+      const result = iniciatedObject.find(even);
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+});
