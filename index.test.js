5fbac02b00f8e61972f53bab85b452d51b4a0143
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-20 22:43:40 +0100

    Refacored test to actuall test (had wrong error msg)

--- index.test.js
+++ index.test.js
@@ -1,9 +1,9 @@
-import { even, MockArray } from "./index.js";
+import { MockArray } from "./index.js";
 
 describe("Given MockArray", () => {
   describe("When it recives 3", () => {
     test("It should throw and error with the msg 'Input object lenght cannot be 0'", () => {
-      const expectedError = "Input object lenght cannot be 0";
+      const expectedError = "Input is not an object!";
       const valueToGive = 3;
 
       expect(() => {
