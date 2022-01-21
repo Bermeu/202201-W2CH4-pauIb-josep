eea91cfc03206817f3885bd9aa3912810df16c01
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-21 01:13:57 +0100

    Changed filter.test.js to use js modules

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,17 +1,4 @@
-class MockArray {
-  obj = {};
-
-  filter(actingFunc) {
-    let returnedObject = {};
-
-    Object.keys(this.obj).forEach((key) => {
-      if (actingFunc(this.obj[key])) {
-        returnedObject.push({ "`${this.obj[key]}`": this.obj[key] });
-      }
-    });
-    return returnedObject;
-  }
-}
+import { MockArray } from "../index";
 
 describe("Given a filter method and obj={a0:1, a1:2}", () => {
   describe("When it (x) => x+2", () => {
